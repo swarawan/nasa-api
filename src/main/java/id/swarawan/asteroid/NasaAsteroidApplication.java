5e6490a4770c575b86a59428bcba85b679e6fe9d
Rio Swarawan Putra <rioswarawan@alienator-2.local>
2024-05-28 08:43:27 +0700

    [RIO] project initialization

---
+++ src/main/java/id/swarawan/asteroid/NasaAsteroidApplication.java
@@ -0,0 +1,13 @@
+package id.swarawan.asteroid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaAsteroidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaAsteroidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/swarawan/asteroid/NasaAsteroidApplicationTests.java
@@ -0,0 +1,13 @@
+package id.swarawan.asteroid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaAsteroidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
